6f3835d6dc7f17759fcfe17b105033150735a1ba
davlatsultonov <davlatsultonov2001@gmail.com>
2021-06-04 00:26:43 +0500

    обновил верстку и стили блока info-block

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,3 @@
-
 $(function () {
     AOS.init({
         once: true
@@ -14,7 +13,7 @@ $(function () {
 
     let isValidMask = false;
 
-    $('.phone-mask').mask('+7 (000) 000-00-00', {
+/*    $('.phone-mask').mask('+7 (000) 000-00-00', {
         onComplete: function (cep) {
             isValidMask = true;
         },
@@ -44,5 +43,32 @@ $(function () {
                 }
             })
         }
-    });
+    });*/
+
+    // accordion blocks
+    function makeAccordion(els, elsParent, activeClass, borderCancelClass) {
+        els.on('click', function () {
+            const thisParent = $(this).parent();
+            const thisParentPrevSibling = thisParent.prev();
+
+            if (thisParent.hasClass(activeClass)) {
+                thisParent.removeClass(activeClass);
+                if (thisParentPrevSibling.length) thisParentPrevSibling.removeClass(borderCancelClass);
+                return;
+            }
+
+            elsParent.each(function () {
+                $(this).removeClass(activeClass);
+                $(this).removeClass(borderCancelClass);
+            })
+
+            thisParent.addClass(activeClass);
+            thisParentPrevSibling.addClass(thisParentPrevSibling.length ? borderCancelClass : '');
+        });
+    }
+
+    // dom els for accordion
+    const infoBlockItemEls = $('.info-block-item');
+    const infoBlockItemBtnEls = infoBlockItemEls.find('.info-block-item__top');
+    makeAccordion(infoBlockItemBtnEls, infoBlockItemEls, 'info-block-item--active', 'info-block-item--no-border');
 });
